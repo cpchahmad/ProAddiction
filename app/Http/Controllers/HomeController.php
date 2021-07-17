1be11afdfe634889088e80097cb425aa96547d9a
salman432 <salman.ali@tetralogicx.com>
2021-07-17 17:11:31 +0500

    commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,9 +31,10 @@ class HomeController extends Controller
     {
         if (isset(Auth::user()->customer_id)) {
             $agent = Customer::find(Auth::user()->customer_id);
+
             $agent_coupen = $agent->coupon_code;
             $agent_orders = Order::where('coupon_code', $agent_coupen)->get();
-
+            dd($agent_orders);
             $total_sales = $agent_orders->sum('total_price');
             $total_commission = ($agent->commission / 100 ) * $total_sales;
             $store_total_orders = Order::get()->count();
