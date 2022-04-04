fd798444f8fdc997b30be23e8c3a79a9f85661fa
Qadeer <82930325+qadeer-1@users.noreply.github.com>
2022-04-04 10:07:46 +0500

    new changes

--- resources/views/professional_form.blade.php
+++ resources/views/professional_form.blade.php
@@ -119,8 +119,6 @@
                     <label for="#">Upload file - Stylist License</label>
                     <input type="file" name="file" class="form-control" id="upload_license" required >
                 </div>
-
-
                 <br>
                 <div class="form-group">
                     <input type="submit" class="btn tab_btn" value="Submit">
@@ -129,7 +127,6 @@
         </div>
     </div>
 </div>
-
 {{--<script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>--}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
@@ -139,7 +136,6 @@
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 <script src="{{asset('/')}}assets/toaster.min.js"></script>
 <script src="{{asset('/')}}country_picker/js/countrySelect.js"></script>
-
 <script>
     $("#country_selector").countrySelect({
         // defaultCountry: "jp",
@@ -176,11 +172,8 @@
 @if(Session::has('error'))
     toastr.error("{{ Session::get('error') }}");
     @endif
-        </script>
+</script>
+@yield('scripts')
+</body>
 
-    @yield('scripts')
-
-
-    </body>
-
-    </html>
+</html>
